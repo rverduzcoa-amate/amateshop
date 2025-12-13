f04af8d9319767a5093804c030fee9ac873a5659
raymu <raymu@example.com>
2025-12-13 14:23:59 -0700

    Fix: set BrowserRouter basename to PUBLIC_URL for GitHub Pages subpath

--- src/index.js
+++ src/index.js
@@ -9,7 +9,7 @@ import './index.css';
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <BrowserRouter>
+    <BrowserRouter basename={process.env.PUBLIC_URL}>
       <Header />
       <App />
     </BrowserRouter>
